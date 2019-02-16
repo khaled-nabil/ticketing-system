64c5edb20bc0afb87160490778f63d63fc27dbc4
khaled-nabil <khaled.nabil.k@gmail.com>
2019-02-16 20:36:20 +0300

    simple verification of id using jwt

--- authentication/index.js
+++ authentication/index.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken');
+const {SECRET} = require('../constants/authentication');
+
 const getUserByToken = (token = null) => {
     if(token) {
-        return "hell4o@gmail.com";
+        return jwt.verify(token,SECRET, function(err, decoded) {
+            if (err) {
+                return null;
+            }else{
+                return decoded.id;
+            }
+        });
     }
     return null;
 };
