2f4543b3729cd20abbbfbfbfcc4f5efc0d06f30b
khaled-nabil <khaled.nabil.k@gmail.com>
2019-08-30 17:07:10 +0200

    eslint configurations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory'; ;;;;;;
